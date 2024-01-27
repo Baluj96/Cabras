02c4f085c3c38bd253ec4c68730481c6c4421e34
Baluj96 <110256191+Baluj96@users.noreply.github.com>
2024-01-27 14:03:23 +0100

    way

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,7 +9,6 @@ public class GameManager : MonoBehaviour
     [Header("Game Manager")]
     public static GameManager instance;
 
-
     [Header("Enemy Manager")]
     GameObject spawnGoat;
     public int level;
@@ -34,6 +33,8 @@ public class GameManager : MonoBehaviour
         DesactivePanels();
 
         spawnGoat = GameObject.FindGameObjectWithTag("Spawner");
+
+        WayManager.instance.StartGame();
 
         Level0();
     }
--- Assets/Scripts/WayManager.cs
+++ Assets/Scripts/WayManager.cs
@@ -4,16 +4,32 @@ using UnityEngine;
 
 public class WayManager : MonoBehaviour
 {
-    [SerializeField] float waitTime = 0.1f;
-    [SerializeField] float dificultad = 0.1f;
-    public GameObject[] wayPrefabs;
-    GameObject sun, goal;
+    public static WayManager instance;
 
-    void Start()
+    public float waitTime = 0.1f;
+    public float dificultad = 0.1f;
+    public GameObject[] wayPrefabs;
+    GameObject sun, goal, player;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void StartGame()
     {
         sun = GameObject.FindGameObjectWithTag("Sun");
-        goal = GameObject.FindGameObjectWithTag("Goal");
         StartCoroutine(GiroLuz());
+
+        goal = GameObject.FindGameObjectWithTag("Goal");
+        player = GameObject.FindGameObjectWithTag("Player");
+        goal.transform.position = new Vector3(90 / dificultad, 0, 0);
+        int d = Mathf.RoundToInt(Vector3.Distance(player.transform.position, goal.transform.position));
+        for (int i = -15; i < d; i += 10)
+        {
+            int r = Random.Range(0, wayPrefabs.Length);
+            Instantiate(wayPrefabs[r], new Vector3(0, 0, i), Quaternion.identity);
+        }
     }
 
     IEnumerator GiroLuz()
